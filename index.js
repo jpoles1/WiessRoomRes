78d9261604ef7ecfb69454d902743c2e6465a0fb
Jordan Poles <jpoles1@gmail.com>
2016-01-21 10:24:48 -0600

    Some mongo stuff working

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.use('/res', express.static('res'));
 app.set('view engine', 'handlebars');
 //Load .env file config
 require('dotenv').config();
+//Load mongodb
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI);
+var Reservation = mongoose.model("Reservation", {
+  reserver: String,
+  email: String,
+  phone: Number,
+  org: String,
+  room: String,
+  start: Date,
+  end: Date
+})
 //Load GCal API Libs
 /*var google = require('googleapis');
 var googleAuth = require('google-auth-library');
@@ -22,6 +34,11 @@ app.get("/", function(req, res){
 })
 app.post("/createRes", function(req, res){
   console.log(req.body)
+  var newres = new Reservation({ reserver: req.body.name, email: req.body.email,  room: req.body.roomName});
+  newres.save(function (err) {
+    if (err) console.log(err)
+    console.log('Adding a new entry right meow');
+  });
   res.send("Form Sent.")
 });
 port = process.env.PORT || 8000;
