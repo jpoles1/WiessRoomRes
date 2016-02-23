b16b6c3ac0659a76196fbef3b8fac7d0518c3850
Jordan Poles <jpoles1@gmail.com>
2016-02-23 13:10:55 -0600

    added conflict checking for already-added events.

---
+++ res/js/approval.js
@@ -0,0 +1,79 @@
+Handlebars.registerHelper('time', function(context) {
+  d = new Date(context);
+  return d.toLocaleString();
+});
+function initFullCall(){
+  $('#cal').fullCalendar({
+    timezone: 'America/Chicago',
+    ignoreTimezone: false,
+    header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+    googleCalendarApiKey: "AIzaSyB3QonsQFYrMDQAi8lez34l0IMpOSPBmhg",
+    events: {
+        googleCalendarId: 'ig0tgdisvlpgbjsp9np5g03474@group.calendar.google.com',
+        className: "gcal-event"
+    },
+    defaultView: "agendaWeek",
+		selectable: true,
+    eventLimit: true, // allow "more" link when too many events
+  })
+}
+getEvents = function(email){
+  $.post("getEvents", {"email": email}, function(rawjson){
+    jsondata = JSON.parse(rawjson);
+    console.log(jsondata);
+    eventdata = jsondata["eventjson"]
+    conflicted = jsondata["conflicted"]
+    //Add Conflict Events
+    var source = $("#conflict-template").html();
+    var template = Handlebars.compile(source);
+    $("#conflict").html(template(jsondata));
+    //Add non-conflicted
+    source = $("#noconflict-template").html();
+    template = Handlebars.compile(source);
+    $("#noconflict").html(template(jsondata));
+    //Add badges to tabs
+    $("#noconflict-badge").html(jsondata["no_conflict"].length)
+    $("#conflict-badge").html(jsondata["conflicted"].length)
+    for(ev_id in eventdata){
+      ev = eventdata[ev_id]
+      eventOpts = {
+        "id": ev["_id"],
+        "title": ev["eventName"]+"\n\n"+ev ["room"],
+        "start": ev["start"],
+        "end": ev["end"]
+      }
+      if(conflicted.indexOf(ev_id)==-1){
+        eventOpts["color"] = "#008A40";
+      }
+      else{
+        //eventOpts["color"] = "#8A004A";
+        eventOpts["color"] = "#8A004A";
+      }
+      $('#cal').fullCalendar('renderEvent', eventOpts, true); // stick? = true
+    }
+    $(".btn-success").click(function(){
+      event_id = ($(this).parent().parent().attr("id"));
+      eventobj = eventdata[event_id];
+      gapi.client.load('calendar', 'v3', calVerify(function(calid){
+        return addToGCal(calid, eventobj["start"], eventobj["end"], eventobj["eventName"], email)
+      }));
+    })
+    $(".btn-danger").click(function(){
+      var event_id = $(this).parent().parent().attr("id");
+      $.post("/rejectEvent", {"eventid": event_id, "email": email}, function(resp){
+        console.log(email);
+        getEvents(email);
+        initFullCall();
+      });
+    })
+  });
+}
+$(function(){
+  var added = 0;
+  initFullCall();
+  $("#tablist a").tab("show")
+})
--- routing/approve_logic.js
+++ routing/approve_logic.js
@@ -44,36 +44,40 @@ module.exports = function(app, Reservation){
   });
   app.post("/getEvents", function(req, res){
     var email = req.body.email;
-    Reservation.find({"rejected": { $ne: email }, "added": { $ne: email } }, function(err, reslist){
+    Reservation.find({"rejected": { $ne: email }/*, "added": { $ne: email }*/}, function(err, reslist){
       conflicted = [];
       no_conflict = [];
       conflict_list = []
       eventlist = {};
-      reslist.forEach(function(res, res_index){
-        conflict_group = [res];
+      reslist.forEach(function(res, res_index, res_array){
+        not_added = res["added"].indexOf(email)==-1;
+        conflict_group = [{editable: not_added, res: res}];
         if(conflicted.indexOf(res["_id"])==-1){
           reslist.slice(res_index+1).forEach(function(elem, check_index){
             check_index+=1;
             function isConflict(element, index, array) {
-              return (element["start"]-elem["end"]<=0) && (elem["start"]-element["end"]<=0) && (elem["room"] == element["room"])
+              return (element["res"]["start"]-elem["end"]<=0) && (elem["start"]-element["res"]["end"]<=0) && (elem["room"] == element["res"]["room"])
             }
             if(conflict_group.some(isConflict) && conflicted.indexOf(check_index)==-1){
-              conflicted.push(elem["_id"])
-              conflict_group.push(elem)
+              if(elem["added"].indexOf(email)==-1){
+                conflicted.push(elem["_id"])
+              }
+              conflict_group.push({"editable": elem["added"].indexOf(email)==-1, "res": elem})
               if(conflict_group.length==2){
-                conflicted.push(res["_id"])
+                if(not_added){conflicted.push(res["_id"])}
               }
             }
           })
           if(conflict_group.length>1){
             conflict_list.push(conflict_group);
           }
-          else{
+          else if(not_added){
             no_conflict.push(res);
           }
         }
-        eventlist[res["_id"]] = res;
+        if(not_added){eventlist[res["_id"]] = res;}
       });
+      console.log(conflict_list)
       res.json(JSON.stringify({
         "eventjson": eventlist,
         "no_conflict": no_conflict,
